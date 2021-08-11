5c5902d6b24b77a8826f0caa9a73c33720257d8c
Maria Afridi <maria.afridi@gmail.com>
2021-08-10 18:07:57 -0700

    added functionality to tables components

---
+++ front-end/src/tables/TableDisplay.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { freeTable } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+
+const TableDisplay = ({ tables, tablesError }) => {
+  const history = useHistory;
+  const abortController = new AbortController();
+
+  const allTables = tables.map((table) => {
+    const { table_id, table_name, capacity, reservation_id } = table;
+    const finishedReservationHandler = async (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      await freeTable(table_id, abortController.signal);
+      history.toString(0);
+      return () => abortController.abort();
+    };
+    return (
+      <tr key={table_id}>
+        <td>{table_id}</td>
+        <td>{table_name}</td>
+        <td>{capacity}</td>
+        <td data-table-id-status={table_id}>
+          {reservation_id ? "occupied" : "free"}
+        </td>
+        <td>
+          {reservation_id ? (
+            <button
+              className="btn btn-warning"
+              type="button"
+              data-table-id-finish={table_id}
+              onClick={(e) => {
+                const confirmation = window.confirm(
+                  `Is this table ready to seat new guests? This cannot be undone.`
+                );
+                return confirmation ? finishedReservationHandler(e) : null;
+              }}
+            >
+              Finish
+            </button>
+          ) : null}
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <ErrorAlert error={tablesError} />
+      <table>
+        <thead thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Table Name</th>
+            <th scope="col">Capacity</th>
+            <th scope="col">Status</th>
+            <th scope="col">Reservation ID</th>
+            <th scope="col">Finish</th>
+          </tr>
+        </thead>
+        <tbody>{allTables}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableDisplay;
---
+++ front-end/src/tables/TablesForm.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { createTable, listTables } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
+
+const TableForm = ({ setTables }) => {
+  const initialFormData = {
+    table_name: "",
+    capacity: "",
+  };
+
+  const history = useHistory;
+  const [formData, setFormData] = useState({ ...initialFormData });
+  const [error, setError] = useState(null);
+  const abortController = new AbortController();
+
+  function formChangeHandler({ target: { name, value } }) {
+    setFormData((previousTable) => ({
+      ...previousTable,
+      [name]: value,
+    }));
+  }
+
+  const validateFields = () => {
+    let foundError = [];
+
+    if (formData.table_name === "") {
+      foundError = { message: `Please fill out table name.` };
+    } else if (!formData.capacity) {
+      foundError = { message: `Please fill out table capacity.` };
+    } else if (formData.table_name.length < 2) {
+      foundError = { message: `Table name must be at least 2 characters.` };
+    }
+    setError(foundError);
+    return foundError.length === 0;
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (validateFields()) {
+      await createTable(formData, abortController.signal);
+      await listTables(abortController.signal).then(setTables);
+      history.push(`/dashboard`);
+      return () => abortController.abort();
+    }
+  };
+
+  return (
+    <main>
+      <h1>Create New Table</h1>
+      <form onSubmit={submitHandler}>
+        <ErrorAlert error={error} />
+        <div className="row mb-3">
+          <div className="col-6 form-group">
+            <label className="form-label" htmlFor="table_name">
+              Table Name
+            </label>
+            <input
+              className="form-control"
+              id="table_name"
+              name="table_name"
+              type="text"
+              minLength="2"
+              value={formData.table_name}
+              onChange={formChangeHandler}
+              required
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <div className="col-6 form-group">
+            <label className="form-label" htmlFor="capacity">
+              Capacity
+            </label>
+            <input
+              className="form-control"
+              id="capacity"
+              name="capacity"
+              type="number"
+              aria-label="Table Capacity"
+              max="12"
+              min="1"
+              value={formData.capactiy}
+              onChange={formChangeHandler}
+              required
+            />
+          </div>
+        </div>
+        <button className="btn btn-primary mx-1" type="submit">
+          <span className="oi-oi-check" />
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+};
+
+export default TableForm;
